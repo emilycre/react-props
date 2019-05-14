de35e51a929bac8c7a6f8139239953b1fcd2c7b7
Emily Creamer <emily.creamer@icloud.com>
2019-05-14 10:44:14 -0700

    Renders a ColorPicker, test passing

---
+++ src/components/ColorPicker.test.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import ColorPicker from './ColorPicker';
+
+describe('ColorPicker Component', () => {
+  it('Renders a ColorPicker', () => {
+    const wrapper = shallow(<ColorPicker />);
+    expect(wrapper).toMatchSnapshot();
+  });
+})
